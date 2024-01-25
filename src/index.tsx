38a57e924bf71249db66cd5c22264765d74d4de8
ksufik <naomi355@yandex.ru>
2024-01-25 23:00:49 +0400

    import i18n in index fix

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import App from './1app/App'
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from '1app/providers/ThemeProvider'
 // библиотека для интренализации приложения (подключения разных языков)
-// import '6shared/config/routeConfig/i18n/i18n'
+import '6shared/config/i18n/i18n'
 import { ErrorBoundary } from '1app/providers/ErrorBoundary'
 import { PageError } from '3widgets/PageError'
 
