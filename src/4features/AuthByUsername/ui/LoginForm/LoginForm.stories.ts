0efa13276bd05de3dd4ad2580e68071b9444597c
ksufik <naomi355@yandex.ru>
2024-02-08 01:31:08 +0400

    add stories

---
+++ src/4features/AuthByUsername/ui/LoginForm/LoginForm.stories.ts
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Theme } from '1app/providers/ThemeProvider'
+import { ThemeDecorator } from '6shared/config/storybook/ThemeDecorator/ThemeDecorator'
+import { LoginForm } from './LoginForm'
+
+const meta = {
+  title: 'features/LoginForm',
+  component: LoginForm,
+  tags: ['autodocs']
+
+} satisfies Meta<typeof LoginForm>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Light: Story = {
+  args: {
+  }
+}
+
+export const Dark: Story = {
+  args: {
+  },
+  decorators: [ThemeDecorator(Theme.DARK)]
+}
---
+++ src/6shared/ui/Input/Input.stories.ts
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Theme } from '1app/providers/ThemeProvider'
+import { ThemeDecorator } from '6shared/config/storybook/ThemeDecorator/ThemeDecorator'
+import { Input } from './Input'
+
+const meta = {
+  title: 'shared/Input',
+  component: Input,
+  tags: ['autodocs'],
+  args: {
+    value: 'text',
+    placeholder: 'Введите текст'
+  }
+
+} satisfies Meta<typeof Input>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Light: Story = {
+  args: {
+  }
+}
+
+export const Dark: Story = {
+  args: {
+  },
+  decorators: [ThemeDecorator(Theme.DARK)]
+}
