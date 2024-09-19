e7c57191cd798ac56eb38c32b969be0bbc8120fd
ksufik <naomi355@yandex.ru>
2024-09-19 22:53:03 +0400

    add interceptors

--- src/6shared/api/api.ts
+++ src/6shared/api/api.ts
@@ -2,8 +2,15 @@ import { USER_LOCALSTORAGE_KEY } from '6shared/const/localstorage'
 import axios from 'axios'
 
 export const $api = axios.create({
-  baseURL: __API__,
-  headers: {
-    authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) ?? ''
+  baseURL: __API__
+  // headers: {
+  //   authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) ?? ''
+  // }
+})
+
+$api.interceptors.request.use((config) => {
+  if (config.headers !== undefined) {
+    config.headers.Authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) ?? ''
   }
+  return config
 })
