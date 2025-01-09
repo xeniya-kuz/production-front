b8d8fc53fd1a5bda8ca0c8422e7cd136f46dbde6
ksufik <naomi355@yandex.ru>
2025-01-10 00:25:18 +0400

    AdminPanelPage fix

--- src/2pages/AdminPanelPage/ui/AdminPanelPage/AdminPanelPage.tsx
+++ src/2pages/AdminPanelPage/ui/AdminPanelPage/AdminPanelPage.tsx
@@ -1,19 +1,13 @@
-import { classNames } from '6shared/lib/classNames/classNames'
-import styles from './AdminPanelPage.module.scss'
 import { memo } from 'react'
 import { Page } from '3widgets/Page'
 import { useTranslation } from 'react-i18next'
 
-interface AdminPanelPageProps {
-  className?: string
-}
-
 const AdminPanelPage = memo(function AdminPanelPage
-({ className }: AdminPanelPageProps): JSX.Element {
+(): JSX.Element {
   const { t } = useTranslation('profile')
 
   return (
-      <Page className={classNames(styles.adminpanelpage, [className])}>
+      <Page >
           {t('admin-panel')}
       </Page>
   )
